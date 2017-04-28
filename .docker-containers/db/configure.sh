998602a31bb50edb8916004e290f2b9d8828ddeb
Alexander Melnik <melnik@impsite.com>
2017-04-28 14:48:16 +0300

    Modified docker files

--- .docker-containers/db/configure.sh
+++ .docker-containers/db/configure.sh
@@ -13,7 +13,6 @@ echo "shared_buffers = $POSTGRES_CONFIG_SHARED_BUFFERS" >> /var/lib/postgresql/d
 echo "effective_cache_size = $POSTGRES_CONFIG_EFFECTIVE_CACHE_SIZE" >> /var/lib/postgresql/data/postgresql.conf
 echo "work_mem = $POSTGRES_CONFIG_WORK_MEM" >> /var/lib/postgresql/data/postgresql.conf
 echo "maintenance_work_mem = $POSTGRES_CONFIG_MAINTENANCE_WORK_MEM" >> /var/lib/postgresql/data/postgresql.conf
-echo "checkpoint_segments = $POSTGRES_CONFIG_CHECKPOINT_SEGMENTS" >> /var/lib/postgresql/data/postgresql.conf
 echo "checkpoint_completion_target = $POSTGRES_CONFIG_CHECKPOINT_COMPLETION_TARGET" >> /var/lib/postgresql/data/postgresql.conf
 echo "wal_buffers = $POSTGRES_CONFIG_WAL_BUFFERS" >> /var/lib/postgresql/data/postgresql.conf
 echo "default_statistics_target = $POSTGRES_CONFIG_DEFAULT_STATISTICS_TARGET" >> /var/lib/postgresql/data/postgresql.conf
